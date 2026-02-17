f3aadda8807b47d57ca88c49cbf475ecebd04403
sreekanthkairuppala <sreekanthkairuppala@gmail.com>
2026-02-17 22:00:46 +0530

    assignments - Arrays

---
+++ src/assignments/Arrays.java
@@ -0,0 +1,28 @@
+package assignments;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+	String [][][] semester = {
+			{
+		
+			
+				
+			{"Data structures", "88"},
+			{"Discrete mathematics","81"},
+			{"Digital economics","76"},
+			{"Operating systems","92"},
+			{"Signals and systems","85"},
+			{"Object oriented program","78"},
+			
+			},
+			
+	};
+	
+	System.out.println("semester 0 - subject 4 Name : "+semester [0][3][0]);
+	System.out.println("semester 0 - subject 5 Name : "+semester [0][4][0]);
+	}};
+			
+			
+	
+
