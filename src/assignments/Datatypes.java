8decc1a8855798cd4e8f107c34a482aef4c5ad80
sreekanthkairuppala <sreekanthkairuppala@gmail.com>
2026-07-21 20:39:52 +0530

    assignments - Datatypes

---
+++ src/assignments/Datatypes.java
@@ -0,0 +1,102 @@
+package assignments;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+	
+		float city_temperature = 25.5f;
+		//System.out.println(city_temperature);
+		System.out.printf("%f,%n",city_temperature);
+		
+		boolean placed_an_order = true;
+		//System.out.println(placed_an_order);
+		System.out.printf("%b,%n",placed_an_order);
+		
+		int phone_number = 123-456-789;
+		System.out.println(phone_number);
+		
+		double coustmers_bank_account = 1000.50;
+		System.out.println(coustmers_bank_account);
+		
+		String email_address = "john.doe@example.com";
+	//	System.out.println(email_address);
+		System.out.printf("%S,%n",email_address);
+		
+		double latitude = 37.7749;
+		System.out.println(latitude);
+		
+		double longtitutde = -122.4194;
+		System.out.println(longtitutde);
+		
+		boolean marital_status = true;
+		System.out.println(marital_status);
+		
+		String occupation = "SoftwareEnginner";
+		System.out.println(occupation);
+		
+		String favourite_colour = "Blue";
+		System.out.println(favourite_colour);
+		
+		long curent_year = 2023;
+		System.out.println(curent_year);
+		
+		int NumberOfFollowers = 100000;
+		//System.out.println(NumberOfFollowers);
+		System.out.printf("%d,%n",NumberOfFollowers);
+		
+		double movie_rating = 7.5;
+		//System.out.println(movie_rating);
+		System.out.printf("%f,%n",movie_rating);
+		
+		char  BloodType = 'A';
+		System.out.println(BloodType);
+		
+		String TitleofBook = "To Kill a Mockingbird";
+		System.out.println(TitleofBook);
+		
+		short No_of_employees = 500;
+		System.out.println(No_of_employees);
+		
+		String Time_of_an_Event = "2.30PM";
+		System.out.println(Time_of_an_Event);
+		
+		String Country = "INDIA";
+		//System.out.println(Country);
+		System.out.printf("%S,%n",Country);
+
+		String eye_colour = "Black";
+		System.out.println(eye_colour);
+		
+		String Person_Birthplace = "Adoni";
+		System.out.println(Person_Birthplace);
+		
+		double Distance = 200.5;
+		System.out.println(Distance);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+	}
+
+}
